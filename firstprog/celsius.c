6ff253e2a7573960e164be4c1e26381966daa7c9
Alkai1 <101726@glr.nl>
2025-10-19 15:00:58 +0200

    first codes

---
+++ firstprog/celsius.c
@@ -0,0 +1,16 @@
+// converts a Fahrenheit temperature to Celsius
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void) {
+    float fahrenheit, celsius;
+
+    printf("Enter Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+    printf("Celius Equivalent: %.1f\n", celsius);
+}
---
+++ firstprog/pun.c
@@ -0,0 +1,14 @@
+/*Name: pun.c
+ *Purpose: Prints a bad pun.
+ *Programmed by: Malikhai
+ */
+
+//function gets a string called and return the value 0
+#include <stdio.h>
+
+int main(void) {
+    int height;
+    height = 8;
+    printf("Height: %d\n", height);
+    printf("To C, or not to C:\nthat is the question.\n");
+}
